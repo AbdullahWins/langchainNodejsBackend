953d19922f921a1954671be2307f2648de4daa8d
Abdullah Al MahMud <abdudevs@gmail.com>
2023-05-21 17:24:22 +0600

    fixed backend issue

--- server.js
+++ server.js
@@ -1,59 +1,36 @@
-// Imports
-const express = require('express')
-
+const express = require("express");
 const app = express();
-
-const cors = require('cors')
+const cors = require("cors");
 const dotenv = require("dotenv");
-
-// LangChain imports
-const { OpenAI } =  require("langchain");
-const { PromptTemplate } = require("langchain/prompts");
-const { LLMChain } = require("langchain/chains");
+const { OpenAI } = require("langchain");
 
 dotenv.config();
 
 // Middleware
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
-
-// Receiving data from App.js
-let data
-app.post('/', (req, res) => {
-    data = req.body.data
-    console.log(data)
-})
-
-// Async function with LangChain to use the data and a prompt to generate a response
-const prompt = (value) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const model = new OpenAI({ openAIApiKey: process.env.OPENAI_API_KEY, temperature: 0.9 });
-      // The `question` variable is the data received from App.js
-      const template = "PROMPT HERE : {question}?";
-      const prompt = new PromptTemplate({
-        template: template,
-        inputVariables: ["question"],
-      });
-
-      const chain = new LLMChain({ llm: model, prompt: prompt });
-      const response = await chain.call({ question: value });
-
-      resolve(response);
-    } catch (err) {
-      console.log(err);
-    }
-  });
-};
-
-// Sending the response to App.js
-app.get('/', async (req, res) => {
-  const response = await prompt(data);
-  res.json(response);
+app.post("/", async (req, res) => {
+  try {
+    const inputData = req.body.data;
+    console.log("Input data:", inputData);
+    const model = new OpenAI({
+      openAIApiKey: process.env.OPENAI_API_KEY,
+      temperature: 0.9,
+    });
+    const response = await model.call(
+      `What would be a good company name a company that makes ${inputData}?`
+    );
+    console.log("Response data:", response);
+    res.send(response);
+  } catch (error) {
+    console.error("Error processing request:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
-// Change the port if you wish to !
-app.listen(5000, () => {
-  console.log('Server listening on port 5000');
-})
+// Start the server
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
